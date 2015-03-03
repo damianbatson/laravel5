4b2f0aef1e01d7c0e53f70847e0c50c9a7ff389b
damian <damian@omnimedia.co.nz>
2015-03-03 16:05:30 +1300

    add $req again

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -38,7 +38,7 @@ class Handler extends ExceptionHandler {
         }
         else
         {
-            return $this->renderWithWhoops($e);
+            return $this->renderWithWhoops($request, $e);
             // return parent::render($request, $e);
         }
     }
