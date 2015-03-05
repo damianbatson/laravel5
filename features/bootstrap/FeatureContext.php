e88baefcc1dadb28323fc8305bce8dda2eade3b7
damian <damian@omnimedia.co.nz>
2015-03-05 16:37:27 +1300

    add more behat scenarios

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -5,12 +5,17 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
+use PHPUnit_Framework_Assert as PHPUnit;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext extends MinkContext implements Context, SnippetAcceptingContext
 {
+
+    protected $name;
+
+    protected $email;
     /**
      * Initializes context.
      *
@@ -28,11 +33,95 @@ class FeatureContext extends MinkContext implements Context, SnippetAcceptingCon
         
     }
 
-    public function iSignInWithValidCredentials()
+public function iCanDoSomethingWithLaravel()
+    {
+        PHPUnit::assertEquals('.env.behat', app()->environmentFile());
+    }
+
+    /**
+     * @When I register :name :email
+     */
+    public function iRegister($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+
+        $this->visit('auth/register');
+
+        $this->fillField('name', $name);
+        $this->fillField('email', $email);
+        $this->fillField('password', 'password');
+        // $this->fillField('password_confirmation', 'password');
+
+        $this->pressButton('Register');
+    }
+
+    /**
+     * @Then I should have an account
+     */
+    public function iShouldHaveAnAccount()
+    {
+        // $this->assertSignedIn();
+
+        $this->assertPageAddress('/auth/register');
+    }
+
+    /**
+     * @Given I have an account :name :email
+     */
+    public function iHaveAnAccount($name, $email)
+    {
+        $this->iRegister($name, $email);
+
+        $this->visit('auth/logout');
+    }
+
+    /**
+     * @When I sign in
+     */
+    public function iSignIn()
     {
         $this->visit('auth/login');
+
         $this->fillField('email', $this->email);
         $this->fillField('password', 'password');
+
         $this->pressButton('Login');
     }
+
+    /**
+     * @When I sign in with invalid credentials
+     */
+    public function iSignInWithInvalidCredentials()
+    {
+        $this->email = 'invalid@example.com';
+
+        $this->iSignIn();
+    }
+
+    /**
+     * @Then I should be logged in
+     */
+    public function iShouldBeLoggedIn()
+    {
+        $this->assertSignedIn();
+    }
+
+    /**
+     * @Then I should not be logged in
+     */
+    public function iShouldNotBeLoggedIn()
+    {
+        PHPUnit::assertTrue(Auth::guest());
+
+        $this->assertPageAddress('auth/login');
+
+        $this->assertPageContainsText('These credentials do not match our records.');
+    }
+
+    private function assertSignedIn()
+    {
+        PHPUnit::assertTrue(Auth::check());
+    }
+
 }
--- features/example.feature
+++ features/example.feature
@@ -8,14 +8,18 @@ Feature: Laravel Behat Extension
     Given I am on the homepage
     When I follow "Login"
     Then the url should match "/auth/login"
-
+    When I sign in
     Then I should see "login"
 
 
-  Scenario: reg
+  Scenario: Successful Register
     Given I am on "/auth/register"
     When I follow "Register"
     Then the url should match "/auth/register"
     When I press "Register"
     Then I should see "register"
+
+    Scenario: Registration
+    When I register "Bobby Bouwmann" "bobbybouwmann@gmail.com"
+    Then I should have an account
     
